534452c8b14f126a05d2f77b498e52c089fcf4e8
Vitor Camilo <96311453+vitorcamilo@users.noreply.github.com>
2022-02-08 13:03:59 +0000

    requirements

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ import requests
 import time
 import json
 import os
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def hello_world():
-  return '<h1>OK</h1>'
-
-def run():
-  app.run(host="0.0.0.0", port=8000)
-
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
 
 class TelegramBot:
   dadosmsg = ''
